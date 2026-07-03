5252ebbe2d9bc8b2279a76465d3c414c519c639d
Kevin Green <kevin.o.green@protonmail.com>
2026-07-03 18:11:25 -0400

    Add optional ports of call to manifest cards

--- src/App.jsx
+++ src/App.jsx
@@ -201,6 +201,13 @@ function App() {
 
                   {passenger.ports_of_call && (
                     <div className="manifest-extra">
+                      <span className="manifest-label manifest-extra-label">Ports of Call</span>
+                      <p>{passenger.ports_of_call}</p>
+                    </div>
+                  )}
+
+                  {passenger.ports_of_call && (
+                    <div className="manifest-extra">
                       <span className="manifest-label">Ports of Call</span>
                       <p>{passenger.ports_of_call}</p>
                     </div>
